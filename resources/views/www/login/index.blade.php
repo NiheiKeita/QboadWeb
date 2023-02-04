185409036b6db28449425060dcf7cbc7b92885fc
keita nihei <keita.nihei.1996.05.29@gmail.com>
2023-02-05 01:03:45 +0900

    Bootstrap導入した

--- resources/views/www/login/index.blade.php
+++ resources/views/www/login/index.blade.php
@@ -4,19 +4,36 @@
 @include('www.layouts.header')
 
 <main>
-    <article>
-        <div class="side">
-            ログイン
+    <body class="container">
+        <div class="c enter-block">
+            <h2 class="side mt-3">
+                ログイン画面
+            </h2>
+            {{-- <div class="content">
+                <form method="POST" action="{{route('www.login.complete')}}" accept-charset="UTF-8">
+                    @csrf
+                    <p>email</p><input name="email" type="text" value="">
+                    <p>Password：<input name="password" type="text" value=""></p>
+                    <button type="button" class="btn btn-primary">ログイン</button>
+                </form>
+            </div> --}}
+            <div>
+                <form method="POST" action="{{route('www.login.complete')}}">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">メールアドレス</label>
+                        <input type="email" class="form-control" name="email">
+                        {{-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> --}}
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">パスワード</label>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                    <button type="submit" class="btn btn-primary">ログイン</button>
+                </form>
+            </div>
         </div>
-        <div class="content">
-            <form method="POST" action="{{route('www.login.complete')}}" accept-charset="UTF-8">
-                @csrf
-                <p>email：<input name="email" type="text" value=""></p>
-                <p>Password：<input name="password" type="text" value=""></p>
-                <input type="submit" value="デフォルト">
-            </form>
-        </div>
-    </article>
+    </body>
 </main>
 @include('www.layouts.footer')
 @endsection
