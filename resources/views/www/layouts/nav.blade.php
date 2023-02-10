a7cd65eedc74d211c6fdb1c03f7df27582edea20
keita nihei <keita.nihei.1996.05.29@gmail.com>
2023-02-11 00:53:49 +0900

    メニューの位置微調整

--- resources/views/www/layouts/nav.blade.php
+++ resources/views/www/layouts/nav.blade.php
@@ -5,32 +5,32 @@
 </div>
 <nav class="globalMenuSp">
     <ul>
-        <li class="text-center align-items-center">
-            <a href="{{route('www.question.index')}}">
+        <li class="text-center align-items-center d-flex justify-content-center">
+            <a href="{{route('www.question.index')}}" class="text-start">
                 <i class="bi bi-house-fill"></i>
                 問題一覧
             </a>
         </li>
-        <li class="text-center align-items-center">
-            <a href="{{route('www.serch.index')}}">
+        <li class="text-center align-items-center d-flex justify-content-center">
+            <a href="{{route('www.serch.index')}}" class="text-start">
                 <i class="bi bi-search"></i>
                 検索
             </a>
         </li>
-        <li class="text-center align-items-center">
-            <a href="{{route('www.information.index')}}">
+        <li class="text-center align-items-center d-flex justify-content-center">
+            <a href="{{route('www.information.index')}}" class="text-start">
                 <i class="bi bi-bell-fill"></i>
                 お知らせ
             </a>
         </li>
-        <li class="text-center align-items-center">
-            <a href="{{route('www.account.index')}}">
+        <li class="text-center align-items-center d-flex justify-content-center">
+            <a href="{{route('www.account.index')}}" class="text-start">
                 <i class="bi bi-person-fill"></i>
                 マイメニュー
             </a>
         </li>
-        <li class="text-center align-items-center">
-            <a href="{{route('www.logout')}}">
+        <li class="text-center align-items-center d-flex justify-content-center">
+            <a href="{{route('www.logout')}}" class="text-start">
                 <i class="bi bi-door-closed-fill"></i>
                 ログアウト
             </a>
