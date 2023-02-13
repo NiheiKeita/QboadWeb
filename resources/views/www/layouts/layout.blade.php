1651454c635d314b8ada1f285b16e749bdf389c6
keita nihei <keita.nihei.1996.05.29@gmail.com>
2023-02-14 02:11:32 +0900

    問題一覧のデザインを修正

--- resources/views/www/layouts/layout.blade.php
+++ resources/views/www/layouts/layout.blade.php
@@ -7,7 +7,7 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   {{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> --}}
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"> --}}
   {{-- <link rel="stylesheet" href="{{ '/public/css/thema.css' }}"> --}}
   <link rel="stylesheet" href="{{ '/public/css/style.css' }}">
   <link rel="stylesheet" href="{{ '/public/css/nav.css' }}">
@@ -15,6 +15,7 @@
   <script src="{{ '/public/js/nav.js' }}"></script>
   <script src="{{ '/public/js/main.js' }}"></script>
   <title></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" integrity="sha384-b6lVK+yci+bfDmaY1u0zE8YYJt0TZxLEAFyYSLHId4xoVvsrQu3INevFKo+Xir8e" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 @include('www.layouts.nav')
--- resources/views/www/question/index.blade.php
+++ resources/views/www/question/index.blade.php
@@ -12,15 +12,41 @@
                 </h2> --}}
                 <div >
                     @foreach ($questions as $question)
-                        <a href="{{route('www.question.solve',$question->id)}}" class="text-decoration-none">
+                        {{-- <a href="{{route('www.question.solve',$question->id)}}" class="text-decoration-none"> --}}
                             <div class="kokuban">
-                                <span class="kokuban_title text-center w-100">問題</span>
-                                <div class="kokuban_inn">
-                                    <p>{{$question->question_content}}</p>
-                                    <p class="question_create_user_name text-end">{{$question->user->user_name}}　より</p>
+                                <div class="three_dots js_three_dots text-end me-3 ms-3 d-inline-flex p-2 bd-highlight w-100">
+                                    <div class="text-start ms-3 time_text">
+                                        {{ $question->created_at->format('H:i Y/m/d') }}
+                                    </div>
+                                    <div class="three_dots js_three_dots text-end">
+                                        <i class="bi bi-three-dots"></i>
+                                    </div>
+                                </div>
+                                <a href="{{route('www.question.solve',$question->id)}}" class="text-decoration-none kokuban_a">
+                                    {{-- <span class="kokuban_title text-center w-100">問題</span> --}}
+                                    {{-- <div class="button_wrapper text_wrapper text-center pb-40 ">
+                                        <div class="text-center align-items-center" >
+                                            問題
+                                        </div>
+                                    </div> --}}
+                                    <div class="kokuban_inn text-center">
+                                        <p>{{$question->question_content}}</p>
+                                        <p class="question_create_user_name text-end">{{$question->user->user_name}}　より</p>
+                                    </div>
+                                </a>
+                                <div class="question_icon_area d-inline-flex p-2 bd-highlight w-100 me-3 ms-3">
+                                    <div class="comment_count_icon_in_quesiton w-25">
+                                        <i class="bi bi-chat-text"></i>
+                                    </div>
+                                    <div class="answer_count_icon_in_quesiton w-25">
+                                        <i class="bi bi-mortarboard"></i>
+                                    </div>
+                                    <div class="heart_count_icon_in_quesiton w-25">
+                                        <i class="bi bi-balloon-heart"></i>
+                                    </div>
                                 </div>
                             </div>
-                        </a>
+                        {{-- </a> --}}
                     @endforeach
                     <a href="{{route('www.question.create')}}">
                         <i class="bi bi-plus-circle-fill question_create_button"></i>
@@ -30,5 +56,6 @@
         </div>
     </body>
 </main>
+
 @include('www.layouts.footer')
 @endsection
